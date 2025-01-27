99380532259696bec80ac2f7924de2753ae076d8
Kerimcan Balkan <kerimcanbalkan@gmail.com>
2025-01-27 15:03:56 +0300

    refactor: route handlers return gin.Handler

--- internal/menu/handler.go
+++ internal/menu/handler.go
@@ -19,206 +19,214 @@ import (
 
 var validate = validator.New()
 
-func GetMenu(c *gin.Context, client *db.MongoClient) {
-	var menu []MenuItem
+func GetMenu(client *db.MongoClient) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var menu []MenuItem
 
-	// Get the collection from the database
-	collection := client.GetCollection(config.Env.DatabaseName, "menu")
+		// Get the collection from the database
+		collection := client.GetCollection(config.Env.DatabaseName, "menu")
 
-	// Get context from the request
-	ctx := c.Request.Context()
+		// Get context from the request
+		ctx := c.Request.Context()
 
-	// Find all documents in the menu collection
-	cursor, err := collection.Find(ctx, bson.M{})
-	if err != nil {
-		handleMongoError(c, err)
-		return
+		// Find all documents in the menu collection
+		cursor, err := collection.Find(ctx, bson.M{})
+		if err != nil {
+			handleMongoError(c, err)
+			return
+		}
+		defer cursor.Close(ctx)
+
+		// Decode the results into the menu slice
+		if err := cursor.All(ctx, &menu); err != nil {
+			handleMongoError(c, err)
+			return
+		}
+
+		// Return the menu in the response
+		c.JSON(http.StatusOK, gin.H{
+			"menu": menu,
+		})
 	}
-	defer cursor.Close(ctx)
-
-	// Decode the results into the menu slice
-	if err := cursor.All(ctx, &menu); err != nil {
-		handleMongoError(c, err)
-		return
-	}
-
-	// Return the menu in the response
-	c.JSON(http.StatusOK, gin.H{
-		"menu": menu,
-	})
 }
 
 // CreateMenu creates
-func CreateMenuItem(c *gin.Context, client *db.MongoClient) {
-	c.Request.Body = http.MaxBytesReader(c.Writer, c.Request.Body, 2<<20)
-	// Parse form data
-	name := c.PostForm("name")
-	description := c.PostForm("description")
-	price := c.PostForm("price")
-	category := c.PostForm("category")
+func CreateMenuItem(client *db.MongoClient) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Request.Body = http.MaxBytesReader(c.Writer, c.Request.Body, 2<<20)
+		// Parse form data
+		name := c.PostForm("name")
+		description := c.PostForm("description")
+		price := c.PostForm("price")
+		category := c.PostForm("category")
 
-	// Handle the image upload
-	file, err := c.FormFile("image")
-	if err != nil {
-		fmt.Println(err.Error())
-		if err.Error() == "multipart: NextPart: http: request body too large" {
-			c.JSON(http.StatusRequestEntityTooLarge, gin.H{
-				"error": fmt.Sprintf("Max request body size is %v bytes\n", 2<<20),
-			})
-			return
-		}
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Image file is required"})
-		return
-	}
-
-	mimeType := file.Header.Get("Content-Type")
-
-	// Validate image mime-type is allowable
-	if valid := isAllowedImageType(mimeType); !valid {
-		c.JSON(
-			http.StatusBadRequest,
-			gin.H{"error": "Invalid File format, must be 'image/jpeg' or 'image/png'"},
-		)
-		return
-	}
-
-	// Save the image to the server
-	imagePath := "uploads/" + generateImageName()
-	if err = c.SaveUploadedFile(file, imagePath); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not save image"})
-		return
-	}
-
-	// Convert price to float
-	priceFloat, err := strconv.ParseFloat(price, 32)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid price format"})
-		return
-	}
-
-	// Create a new menu item
-	item := MenuItem{
-		Name:        name,
-		Description: description,
-		Price:       float32(priceFloat),
-		Category:    category,
-		Img:         imagePath,
-	}
-
-	// Validate the struct
-	if err = ValidateMenu(validate, item); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Get the collection
-	collection := client.GetCollection(config.Env.DatabaseName, "menu")
-
-	// Get context from the request
-	ctx := c.Request.Context()
-
-	// Insert the item into the database
-	result, err := collection.InsertOne(ctx, item)
-	if err != nil {
-		handleMongoError(c, err)
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Item added successfully",
-		"item_id": result.InsertedID,
-	})
-}
-
-func DeleteMenuItem(c *gin.Context, client *db.MongoClient) {
-	id := c.Param("id")
-	if id == "" {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Invalid ID!",
-		})
-		return
-	}
-
-	docID, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Invalid ID!",
-		})
-		return
-	}
-
-	// Get collection from db
-	collection := client.GetCollection(config.Env.DatabaseName, "menu")
-
-	// Get context from the request
-	ctx := c.Request.Context()
-
-	// Retrieve the menu item to get the image path
-	var menuItem MenuItem
-	err = collection.FindOne(ctx, bson.M{"_id": docID}).Decode(&menuItem)
-	if err != nil {
-		handleMongoError(c, err)
-		return
-	}
-
-	// Delete the image from the /uploads directory
-	if menuItem.Img != "" {
-		err = os.Remove(menuItem.Img)
+		// Handle the image upload
+		file, err := c.FormFile("image")
 		if err != nil {
-			if !os.IsNotExist(err) {
-
-				c.JSON(http.StatusInternalServerError, gin.H{
-					"error": "Could not remove related files",
+			fmt.Println(err.Error())
+			if err.Error() == "multipart: NextPart: http: request body too large" {
+				c.JSON(http.StatusRequestEntityTooLarge, gin.H{
+					"error": fmt.Sprintf("Max request body size is %v bytes\n", 2<<20),
 				})
 				return
 			}
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Image file is required"})
+			return
 		}
-	}
 
-	// Now delete the menu item from the database
-	_, err = collection.DeleteOne(ctx, bson.M{"_id": docID})
-	if err != nil {
-		handleMongoError(c, err)
-		return
-	}
+		mimeType := file.Header.Get("Content-Type")
 
-	c.JSON(http.StatusOK, nil)
+		// Validate image mime-type is allowable
+		if valid := isAllowedImageType(mimeType); !valid {
+			c.JSON(
+				http.StatusBadRequest,
+				gin.H{"error": "Invalid File format, must be 'image/jpeg' or 'image/png'"},
+			)
+			return
+		}
+
+		// Save the image to the server
+		imagePath := "uploads/" + generateImageName()
+		if err = c.SaveUploadedFile(file, imagePath); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not save image"})
+			return
+		}
+
+		// Convert price to float
+		priceFloat, err := strconv.ParseFloat(price, 32)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid price format"})
+			return
+		}
+
+		// Create a new menu item
+		item := MenuItem{
+			Name:        name,
+			Description: description,
+			Price:       float32(priceFloat),
+			Category:    category,
+			Img:         imagePath,
+		}
+
+		// Validate the struct
+		if err = ValidateMenu(validate, item); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Get the collection
+		collection := client.GetCollection(config.Env.DatabaseName, "menu")
+
+		// Get context from the request
+		ctx := c.Request.Context()
+
+		// Insert the item into the database
+		result, err := collection.InsertOne(ctx, item)
+		if err != nil {
+			handleMongoError(c, err)
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Item added successfully",
+			"item_id": result.InsertedID,
+		})
+	}
 }
 
-func GetMenuByID(c *gin.Context, client *db.MongoClient) {
-	id := c.Param("id")
-	if id == "" {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Invalid ID!",
+func DeleteMenuItem(client *db.MongoClient) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		if id == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid ID!",
+			})
+			return
+		}
+
+		docID, err := primitive.ObjectIDFromHex(id)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid ID!",
+			})
+			return
+		}
+
+		// Get collection from db
+		collection := client.GetCollection(config.Env.DatabaseName, "menu")
+
+		// Get context from the request
+		ctx := c.Request.Context()
+
+		// Retrieve the menu item to get the image path
+		var menuItem MenuItem
+		err = collection.FindOne(ctx, bson.M{"_id": docID}).Decode(&menuItem)
+		if err != nil {
+			handleMongoError(c, err)
+			return
+		}
+
+		// Delete the image from the /uploads directory
+		if menuItem.Img != "" {
+			err = os.Remove(menuItem.Img)
+			if err != nil {
+				if !os.IsNotExist(err) {
+
+					c.JSON(http.StatusInternalServerError, gin.H{
+						"error": "Could not remove related files",
+					})
+					return
+				}
+			}
+		}
+
+		// Now delete the menu item from the database
+		_, err = collection.DeleteOne(ctx, bson.M{"_id": docID})
+		if err != nil {
+			handleMongoError(c, err)
+			return
+		}
+
+		c.JSON(http.StatusOK, nil)
+	}
+}
+
+func GetMenuByID(client *db.MongoClient) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		if id == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid ID!",
+			})
+			return
+		}
+
+		docID, err := primitive.ObjectIDFromHex(id)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid ID!",
+			})
+			return
+		}
+
+		// Get collection from db
+		collection := client.GetCollection(config.Env.DatabaseName, "menu")
+
+		// Get context from the request
+		ctx := c.Request.Context()
+
+		// Retrieve the menu item
+		var menuItem MenuItem
+		err = collection.FindOne(ctx, bson.M{"_id": docID}).Decode(&menuItem)
+		if err != nil {
+			handleMongoError(c, err)
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"item": menuItem,
 		})
-		return
 	}
-
-	docID, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Invalid ID!",
-		})
-		return
-	}
-
-	// Get collection from db
-	collection := client.GetCollection(config.Env.DatabaseName, "menu")
-
-	// Get context from the request
-	ctx := c.Request.Context()
-
-	// Retrieve the menu item
-	var menuItem MenuItem
-	err = collection.FindOne(ctx, bson.M{"_id": docID}).Decode(&menuItem)
-	if err != nil {
-		handleMongoError(c, err)
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"item": menuItem,
-	})
 }
 
 func GetMenuItemImage(c *gin.Context) {
