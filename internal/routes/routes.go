c76e5b0985eddf6ecdd50a0b05aeb35d5a62ca7d
kerimcanbalkan <kerimcanbalkan@gmail.com>
2025-05-08 21:15:11 +0300

    fix: table endpoint permissions to include waiter and cashier

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -74,7 +74,7 @@ func SetupRoutes(r *gin.Engine, client *db.MongoClient) {
 	tableGroup := r.Group("/api/v1/table")
 	{
 		tableGroup.POST("", auth.Authenticate([]string{"admin"}), table.CreateTable(client))
-		tableGroup.GET("", auth.Authenticate([]string{"admin"}), table.GetTables(client))
+		tableGroup.GET("", auth.Authenticate([]string{"admin","waiter", "cashier"}), table.GetTables(client))
 		tableGroup.GET("/:id", table.GetTableById(client))
 		tableGroup.DELETE("/:id", auth.Authenticate([]string{"admin"}), table.DeleteTable(client))
 	}
